7ee6a82112f110cf242e731fd336826321d97373
G <glen@ruddicks.net>
2026-01-27 16:05:33 -0700

    updated code

--- Arena/Assets/scripts/player_movement.cs
+++ Arena/Assets/scripts/player_movement.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 public class PlayerBehavior : MonoBehaviour
 {
-    public float MoveSpeed = 10f;
-    public float RotateSpeed = 75f;
-    private float _vInput;
-    private float _hInput;
-    
+    public float MoveSpeed = 10f, RotateSpeed = 75f, JumpVelocity = 10f;
+    public float DistanceToGround = 0.1f;
+    private float _vInput, _hInput;
+    private bool _isJumping;
     private Rigidbody _rb;
+    public LayerMask GroundLayer;
+    private SphereCollider _col;
     
     void Start()
     {
-        
         _rb = GetComponent<Rigidbody>();
+        _col = GetComponent<SphereCollider>();
     }
     
     void Update()
@@ -21,25 +22,25 @@ public class PlayerBehavior : MonoBehaviour
         _vInput = Input.GetAxis("Vertical") * MoveSpeed;
         _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
         
-        this.transform.Translate(Vector3.forward * _vInput * 
-        Time.deltaTime);
+        this.transform.Translate(Vector3.forward * _vInput * Time.deltaTime);
         this.transform.Rotate(Vector3.up * _hInput * Time.deltaTime);
-        
 
-
+        _isJumping |= Input.GetKeyDown(KeyCode.J);
     }
 
     void FixedUpdate()
     {
+        Vector3 rotation = Vector3.up * _hInput;  
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
         
-        Vector3 rotation = Vector3.up * _hInput;
-        
-        Quaternion angleRot = Quaternion.Euler(rotation *
-            Time.fixedDeltaTime);
-        
-        _rb.MovePosition(this.transform.position +
-            this.transform.forward * _vInput * Time.fixedDeltaTime);
-        
+        _rb.MovePosition(this.transform.position + this.transform.forward * _vInput * Time.fixedDeltaTime);
         _rb.MoveRotation(_rb.rotation * angleRot);
+
+        if (_isJumping)
+        {
+            _rb.AddForce(Vector3.up * JumpVelocity, ForceMode.Impulse);
+        }
+        _isJumping = false;
+
     }
 }
